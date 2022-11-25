cdfd5fbf86c7633cc2c7ee72aa9add35999d649f
aejoia74 <kz43258587@gmail.com>
2022-11-25 18:24:59 +0900

    composerインストール、.env追加、DBの接続情報は.env内へ

--- src/private/app/connect_database.php
+++ src/private/app/connect_database.php
@@ -1,12 +1,21 @@
 <?php
+require_once('../private/vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$db_host = $_ENV['DB_HOST'];
+$db_name = $_ENV['DB_NAME'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
+
 try {
     //データベースへ接続
     $pdo = new PDO(
-        'mysql:host=mysql;dbname=test_db;charset=utf8mb4',
+        'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8mb4',
         //ユーザー名
-        'test_db_docker',
+         $db_user,
         //パス
-        'test_db_docker_pass',
+         $db_pass,
     //PDOのオプションを指定
     [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
