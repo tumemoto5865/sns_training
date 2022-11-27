f875af1cc6a35901dd2f240a27f0a9862a1b42f0
aejoia74 <kz43258587@gmail.com>
2022-11-27 18:43:00 +0900

    footer追記

--- src/app/manage_footer.php
+++ src/app/manage_footer.php
@@ -1,5 +1,23 @@
+<footer>
+    <?php
+    if (isset($_SESSION['login'])) {
+    ?>
+        <p>
+        <form action="index.php" method="post">
+            <input type="hidden" name="logout" value="">
+            <input type="submit" value="ログアウト" class="submit">
+        </form>
+        </p>
+    <?php
+    } else {
+    ?>
+    <p><a href="index.php">index.phpに戻る</a></p>
+    <?php
+    }
+    ?>
+</footer>
 <script>
-  app / script.js
+    app / script.js
 </script>
 </body>
 
